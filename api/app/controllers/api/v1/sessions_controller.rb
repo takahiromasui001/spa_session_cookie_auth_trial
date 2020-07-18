7b1a13ac66e9d1bf34269ff119c2c511b379ff70
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-18 22:31:08 +0900

    api login

--- api/app/controllers/api/v1/sessions_controller.rb
+++ api/app/controllers/api/v1/sessions_controller.rb
@@ -2,7 +2,14 @@ module Api
   module V1
     class SessionsController < ApplicationController
       def create
-        pp 'OK create'
+        user = User.find_by(name: params[:name]).authenticate(params[:password])
+
+        if user.present?
+          session[:user_id] = user.id
+          render json: { message: "login succeed" }
+        else
+          render json: { message: "login failed" }, status: 401
+        end
       end
 
       def delete
