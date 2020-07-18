80512425e9c4b3892c959e54d5dea284d3a48a44
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-18 21:50:06 +0900

    パスワード設定

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'user1', email: 'user1@example.com', password: '1111', password_confirmation: '1111')
