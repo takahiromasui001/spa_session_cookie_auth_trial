4b98cb83939ad308bb362509b9b8f769c514c5a2
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-18 22:00:44 +0900

    Companyモデル追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_123828) do
+ActiveRecord::Schema.define(version: 2020_07_18_125112) do
+
+  create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
