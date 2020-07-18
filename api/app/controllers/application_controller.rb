1b0d535f7775fa7a2ee4e96c82d3213c2489ea3a
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-18 23:52:35 +0900

    login check

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::API
   before_action :login_check
 
   def login_check
-    pp 'login_check'
+    if current_user.blank?
+      render json: { message: "unauthorized" }, status: 401
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
