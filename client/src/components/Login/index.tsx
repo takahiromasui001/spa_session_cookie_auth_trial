e2e76a9d1792fc9da40c0ecb3671e595ebfed17c
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-19 14:13:01 +0900

    login機能実装

--- client/src/components/Login/index.tsx
+++ client/src/components/Login/index.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
-const Login: React.FC = () => <div>Login</div>
+const Login: React.FC = () => {
+  const history = useHistory()
+  const handleLogin = async () => {
+    await axios
+      .post(
+        'http://127.0.0.1:3000/api/v1/login',
+        {
+          name: 'user1',
+          password: '1111',
+        },
+        { withCredentials: true }
+      )
+      .then(() => {
+        history.push('/')
+      })
+      .catch(() => {
+        console.log('login failed')
+      })
+  }
 
+  return (
+    <div>
+      Login
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  )
+}
 export default Login
