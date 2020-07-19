fc20156dbc0873feeabd0cf1eabcd16727a840ac
Takahiro Masui <takahiro.masui@luxiar.com>
2020-07-19 15:55:43 +0900

    logout時の認証情報更新

--- client/src/components/Logout/index.tsx
+++ client/src/components/Logout/index.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import axios from 'axios'
 import { useHistory } from 'react-router-dom'
+import { AuthContext } from '../AuthProvider'
 
 const Logout: React.FC = () => {
   const history = useHistory()
+  const { setAuthInfo } = useContext(AuthContext)
+
   const handleLogout = async () => {
     await axios
       .delete('http://127.0.0.1:3000/api/v1/logout', { withCredentials: true })
       .then(() => {
+        setAuthInfo({ userId: null })
         history.push('/login')
       })
       .catch(() => {
